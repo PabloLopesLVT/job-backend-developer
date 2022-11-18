a3221b758141597a70faa43410fbcecf47d65745
Pablo Lopes <pablo.lopes@levantti.com.br>
2022-11-17 23:05:12 -0300

    set routes, model, requests, controller and migration

---
+++ database/migrations/2022_11_18_014354_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->float('price')->default(0)->nullable('false');
+            $table->text('description')->nullable('false');
+            $table->string('category')->nullable('false');
+            $table->string('image_url')->nullable('true');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
